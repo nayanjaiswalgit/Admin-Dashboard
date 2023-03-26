07bf6d91bfb21756f7aac746264db3ec040e33cf
Nayan <jaiswal2062@gmail.com>
2023-03-26 18:09:27 +0530

    styling

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import {SparklineComponent,Inject, SparklineTooltip} from '@syncfusion/ej2-react-charts'
 
-const SparkLine = () => {
+const SparkLine = ({ id, height, width, colour, data, type, currentColour}) => {
   return (
-    <div>SparkLine</div>
+    <SparklineComponent 
+    id = {id}
+    height = {height}
+    width = {width}
+    lineWidth={1}
+    valueType = "Numeric"
+    fill= {colour}
+    border={{color:currentColour, width:2}}
+    dataSource = {data}
+    xName = "x"
+    yName = "y"
+    type = {type}
+    tooltipSettings = {{visible:true,
+    format: '${x} : data ${y}',
+    trackLineSettings : {
+      visible:true
+    }
+  
+  }}
+    >
+<Inject services={[SparklineTooltip]}/>
+
+    </SparklineComponent>
   )
 }
 
